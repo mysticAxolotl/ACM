d5f415d63c009121f85c759dc25bcd2fa6a9e95b
mysticAxolotl <brighid1681@yahoo.com>
2020-02-14 20:21:25 -0500

    Add A again

---
+++ s2020/2.14/b.cpp
@@ -0,0 +1,28 @@
+// B. Assigning to Classes
+// http://codeforces.com/contest/1300/problem/B
+// 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int tests, num;
+    cin >> tests;
+    for(; tests; --tests )
+    {
+        int num;
+        cin >> num;
+        vector< int > arr ( num * 2 );
+        for( int i = 0; i < num; ++i )
+            cin >> arr[ i ];
+        sort( arr.begin(), arr.end() );
+
+
+    }
+
+    return 0;
+}
