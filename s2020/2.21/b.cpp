38c8ecd90b876165537006e6b972c70fd148661f
mysticAxolotl <brighid1681@yahoo.com>
2020-02-18 12:40:12 -0500

    Add prob B

---
+++ s2020/2.21/b.cpp
@@ -0,0 +1,59 @@
+// B. Longest Palindrome
+// https://codeforces.com/contest/1304/problem/B
+// ACCEPTED
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    short num, len;
+    cin >> num >> len;
+
+    map<string, int> words;
+    string bigP = "";
+    while( num-- )
+    {
+        string hold, hold2;
+        cin >> hold;
+        hold2 = hold;
+        reverse( hold2.begin(), hold2.end() );
+        if( hold == hold2 )
+        {
+            if( hold.length() > bigP.length() )
+                bigP = hold;
+        }
+        else
+        {
+            ++words[ hold ];
+            ++words[ hold2 ];
+        }
+    }
+
+    string pal = "";
+    ++num;
+    for( auto i = words.begin(); i != words.end(); ++i )
+    {
+        if( i->second == 2 )
+        {
+            pal += i->first;
+            num += 2;
+            string hold = i->first;
+            reverse( hold.begin(), hold.end() );
+            words.erase( hold );
+        }
+    }
+    
+    if( bigP.length() != 0 )
+        ++num;
+    cout << num * len << '\n'
+         << pal << bigP;
+    reverse( pal.begin(), pal.end() );
+    cout << pal << '\n';
+
+    return 0;
+}
